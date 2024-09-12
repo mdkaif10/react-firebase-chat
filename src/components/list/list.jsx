c787c9cc5aa2eab39f9d118fb02176816a433aa8
Md Kaif <md.71.kaif@gmail.com>
2024-09-12 23:50:21 +0530

    Co-authored-by: Injamamul Islam <InjamamulIslam@users.noreply.github.com>

---
+++ src/components/list/list.jsx
@@ -0,0 +1,15 @@
+import ChatList from "./chatList/chatList"
+import "./list.css"
+import Userinfo from "./userinfo/userinfo"
+
+const List = () => {
+  return (
+    <div className='list'>
+      <Userinfo/>
+      <ChatList/>
+
+    </div>
+  )
+}
+
+export default List
